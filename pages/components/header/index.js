1f97a768350dda5f51c263b0ee7c232e302625ef
igor <igorpinheir1@gmail.com>
2022-11-22 10:51:16 -0300

    ajuste responsividade

--- pages/components/header/index.js
+++ pages/components/header/index.js
@@ -39,7 +39,7 @@ const Header = () => {
   return (
     <>
       <Container
-        marginTop="3rem"
+        marginTop={isLargerThan1000 ? "3rem" : "0px"}
         maxW="container.xl"
         h="100vh"
         w="100%"
